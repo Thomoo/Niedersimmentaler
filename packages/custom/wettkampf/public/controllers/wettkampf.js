7b22671ac0442e6f676b1da285c293e966466487
Tom <huber.tom@swissonline.ch>
2017-02-11 11:19:40 +0100

    - Wettkampf neues Design

--- packages/custom/wettkampf/public/controllers/wettkampf.js
+++ packages/custom/wettkampf/public/controllers/wettkampf.js
@@ -30,15 +30,18 @@
           });
         };
 
-        $scope.saveWettkampf = function() {
-          $log.info('save Wettkampf...');
-          var wettkampf = new Wettkampf({
-            title : $scope.wettkampf.title,
-            infoTextActive : $scope.wettkampf.infoTextActive,
-            infoTextInactive : $scope.wettkampf.infoTextInactive,
-            anmeldungActive : $scope.wettkampf.anmeldungActive
-          });
-          wettkampf.$save();
+        $scope.saveWettkampf = function(isValid) {
+          if (isValid) {
+            $log.info('save Wettkampf...');
+            var wettkampf = new Wettkampf({
+              title: $scope.wettkampf.title,
+              infoTextActive: $scope.wettkampf.infoTextActive,
+              infoTextInactive: $scope.wettkampf.infoTextInactive,
+              anmeldungActive: $scope.wettkampf.anmeldungActive
+            });
+            wettkampf.$save();
+            $scope.wettkampfForm.$setPristine();
+          }
         };
     }
 
