69b0951399fe1f9e85e5061ddb61b9d4674c7bf5
Tom <huber.tom@swissonline.ch>
2017-02-13 22:24:43 +0100

    - Wettkämpfe als Cards und in eigener Direktive

--- packages/custom/teilnehmer/public/directives/user-form.js
+++ packages/custom/teilnehmer/public/directives/user-form.js
@@ -4,7 +4,6 @@ angular.module('mean.teilnehmer').directive('userForm', ['$log', function () {
   return {
     restrict: 'E',
     scope: {
-      showAward: '=',
       readonly: '=',
       teilnehmer: '=',
       valid: '='
